03c9c383e19563819a437d2f3d7aa78bb3109e73
evgenii-shvetsov <sea.dev91@gmail.com>
2022-11-14 16:22:53 -0800

    Add users controller, sessions controller, implemented CRRLLL

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,17 +19,39 @@ class User < ApplicationRecord
     before_validation :ensure_session_token
 
     def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
     end
 
     def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
     end
 
     def is_password?(password)
+        bcrypt_object = BCrypt::Password.new(self.password_digest)
+        bcrypt_object.is_password?(password)
     end
 
     def reset_session_token
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
     end
 
     def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def generate_unique_session_token
+        token = SecureRandom::urlsafe_base64
+        while User.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64
+        end
+        token
     end
 end
